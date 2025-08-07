9d65513dd43b844d46388470ce48e6901eb1d407
nakamura_kouji <nakamura_kouji@dds.co.jp>
2025-08-07 12:54:09 +0900

    all body message & cors header response

--- echo-mock.js
+++ echo-mock.js
@@ -9,9 +9,28 @@ app.use(express.json());
 // POST /api/echo に対して、message をそのまま返す
 app.post("/api/echo", (req, res) => {
   const message = req.body.message;
+  const source = req.body.source || "unknown";
+  const lineno = req.body.lineno || 0;
+  const colno = req.body.colno || 0;
   res.json({
-    echo: message,
+    message: message,
+    source: source,
+    lineno: lineno,
+    colno: colno,
   });
+  res.header("Content-Type", "application/json");
+  res.header("Access-Control-Allow-Origin", "https://login.dds.click");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", true);
+});
+
+app.options("/api/echo", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://login.dds.click");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.sendStatus(200);
 });
 
 // サーバー起動
